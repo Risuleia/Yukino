bb051327d391441ff337bc79b4fbfb80467e453e
Risuleia <72485538+Risuleia@users.noreply.github.com>
2022-07-15 17:54:54 +0000

    fixes

--- Events/afk.js
+++ Events/afk.js
@@ -1,38 +1,38 @@
-const client = require("../index");
-const moment = require('moment');
+// const client = require("../index");
+// const moment = require('moment');
 
-client.on('messageCreate', async msg => {
+// client.on('messageCreate', async msg => {
 
-  if (!msg.guild || msg.author.bot) return
+//   if (!msg.guild || msg.author.bot) return
 
-  const mention = msg.mentions.members.first();
+//   const mention = msg.mentions.members.first();
 
-  if (mention) {
+//   if (mention) {
 
-    const afk = client.afk.get(mention.id)
+//     const afk = client.afk.get(mention.id)
 
-    if (afk) {
-      const [timestamp, reason] = afk;
-      const timeAgo = moment(timestamp).fromNow();
+//     if (afk) {
+//       const [timestamp, reason] = afk;
+//       const timeAgo = moment(timestamp).fromNow();
 
-      msg.reply({
-        content: `${mention.nickname || mention.user.username} is AFK (${timeAgo}): ${reason}`,
-        allowedMentions: { repliedUser: false }
-      }).then(message => setTimeout(() => message.delete(), 2000))
-    }
+//       msg.reply({
+//         content: `${mention.nickname || mention.user.username} is AFK (${timeAgo}): ${reason}`,
+//         allowedMentions: { repliedUser: false }
+//       }).then(message => setTimeout(() => message.delete(), 2000))
+//     }
 
-  }
+//   }
 
-  const isAFK = client.afk.get(msg.author.id)
-  if (isAFK) {
-    client.afk.delete(msg.author.id);
+//   const isAFK = client.afk.get(msg.author.id)
+//   if (isAFK) {
+//     client.afk.delete(msg.author.id);
 
-    msg.reply(`${msg.member.displayName}, I have removed your AFK!`).then(message => setTimeout(() => message.delete(), 2000));
+//     msg.reply(`${msg.member.displayName}, I have removed your AFK!`).then(message => setTimeout(() => message.delete(), 2000));
 
-    if (msg.guild.me.permissions.has('MANAGE_NICKNAMES') && msg.member.manageable) {
-      msg.member.setNickname(msg.member.nickname.replace('[AFK] ', ''))
-    }
+//     if (msg.guild.me.permissions.has('MANAGE_NICKNAMES') && msg.member.manageable) {
+//       msg.member.setNickname(msg.member.nickname.replace('[AFK] ', ''))
+//     }
 
-  }
+//   }
 
-})
+// })
--- Events/censoring.js
+++ Events/censoring.js
@@ -1,19 +1,19 @@
-const client = require('../index');
-const Database = require("@replit/database");
-const db = new Database();
+// const client = require('../index');
+// const Database = require("@replit/database");
+// const db = new Database();
 
-client.on('messageCreate', msg => {
+// client.on('messageCreate', msg => {
 
-	if (!db.get('censored_words')) return
+// 	if (!db.get('censored_words')) return
 
-  db.get('censored_words').then(words => {
-    if (msg.author.bot) return
-    if (msg.content.startsWith(`${client.config.prefix}censor`)) return
+//   db.get('censored_words').then(words => {
+//     if (msg.author.bot) return
+//     if (msg.content.startsWith(`${client.config.prefix}censor`)) return
 
-    if (words?.some(word => msg.content?.toLowerCase().includes(word))) {
-      msg.delete()
-        .catch((e) => console.log(e))
-    }
-  })
+//     if (words?.some(word => msg.content?.toLowerCase().includes(word))) {
+//       msg.delete()
+//         .catch((e) => console.log(e))
+//     }
+//   })
 
-})
+// })
