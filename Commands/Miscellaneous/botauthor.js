085521e5ea3c2d5dfa39952d67fdf1a2643354bb
Risuleia <72485538+Risuleia@users.noreply.github.com>
2022-11-17 08:56:26 +0000

    minor changes

--- Commands/Miscellaneous/botauthor.js
+++ Commands/Miscellaneous/botauthor.js
@@ -3,6 +3,13 @@ module.exports = {
   aliases: ['botowner', 'author'],
   description: "Displays the creator of the bot and other credits.",
   execute: async (client, message, args, db) => {
-    message.reply({ content: `Created with ♡ by Risuleia#0755!\nDebugged with assistance from mimi'#7777!`, allowedMentions: { repliedUser: false } })
+
+    const risu = client.users.fetch("69362309971050501")
+    const mimi = client.users.fetch("92756667385317391")
+
+    message.reply({ 
+      content: `Created with ♡ by ${risu.username}#${risu.discriminator}!\nDebugged with assistance from ${mimi.username}#${mimi.discriminator}!`,
+      allowedMentions: { repliedUser: false }
+    })
   }
 }
