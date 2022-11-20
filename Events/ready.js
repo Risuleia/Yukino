f56d599594f4fe9a18837d294ab03b7b74eec229
Risuleia <72485538+Risuleia@users.noreply.github.com>
2022-11-20 21:31:36 +0000

    ready event

--- Events/ready.js
+++ Events/ready.js
@@ -2,6 +2,6 @@ const client = require("../index");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('mimi\'s server', { type: 'WATCHING' });
+  client.user.setActivity('i love mimi <333');
   client.user.setStatus('idle');
 });
