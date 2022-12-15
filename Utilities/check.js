8db63869fdec8905822d365f87756a5ea5667706
Risuleia <72485538+Risuleia@users.noreply.github.com>
2022-12-15 17:33:56 +0530

    Update check.js

--- Utilities/check.js
+++ Utilities/check.js
@@ -6,7 +6,7 @@ const check = async () => {
 	const mariposa = client.guilds.cache.find(g => g.id == '879726321155051530')
 	const embeds = await db.get('embeds')
 	const conf = await db.get('serverconf')
-	Object.keys(conf).forEach(key => {
+	await Object.keys(conf).forEach(key => {
 		if (!(mariposa.roles.cache.some(r => r.id === conf[key]) || mariposa.channels.cache.some(c => c.id === conf[key]) || embeds.some(emb => emb.name === conf[key]))) conf[key] = null
 		else return
 	})
