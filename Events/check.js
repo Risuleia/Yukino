af66dd699fff616ca22966f42b6ffe91ae4e0101
Risuleia <72485538+Risuleia@users.noreply.github.com>
2022-12-16 14:50:28 +0000

    minor fixes

--- Events/check.js
+++ Events/check.js
@@ -1,9 +1,6 @@
 const client = require("../index");
 const check = require("../Utilities/check");
 
-client.once('ready', async client => {
-    check()
-})
 client.on('channelDelete', async channel => {
     check()
 })
